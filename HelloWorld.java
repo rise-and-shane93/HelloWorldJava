a5deaa67946e2e33baac3d8b45d23654f43daf13
Shane Harper <cdtharper@gmail.com>
2021-02-11 07:43:01 -0500

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 6;
+
+        int remainder = x % y;
+
+        System.out.println("\n" + "z = " + remainder);
+    }
+}
