5d008993d46c8d128d471811ea5cadee71d691dc
Shane Harper <cdtharper@gmail.com>
2021-02-12 06:59:55 -0500

    first test with cast operation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int x = 3;
-        int y = 2;
-        float w = 2F;
-        float z = 1F;
-        float q;
+        int x;
+        double d = 260.99;
 
-        q = w / z; // Uniform mode expression
+        x = (int) d; // A cast operation
 
-        z = x / y; // Mixed mode expression
-        System.out.println("\n" + z);
-
-        z = x / w; // Another mixed mode expression
-        System.out.println(z);
+        System.out.println("\n" + "x = " + x + "\n");
     }
 }
