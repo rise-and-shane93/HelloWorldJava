0a76bab39d41d86f760e509247cde401564005bc
Shane Harper <cdtharper@gmail.com>
2021-02-23 21:53:20 -0500

    trying string equality operators

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,43 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int x;
-        double d = 260.99;
+        
+        // int[][] myIntArray = {{5, 10}, {15, 20, 25}};
 
-        x = (int) d; // A cast operation
+        // for (int i=0; i < myIntArray.length; i++) {
+        //     for (int j = 0; j < myIntArray[i].length; j++) {
+        //         System.out.println(myIntArray[i][j]);
+        //     }
+        // }
 
-        System.out.println("\n" + "x = " + x + "\n");
+        String catName = "Shane";
+        String catName2 = "Shane";
+        String catName3 = new String("Shane");
+        int shaneAge = 27;
+
+        System.out.println(catName + " is " + shaneAge + " years old.");
+
+        if (catName == "Shane") {
+            System.out.println("catName is equal to \"Shane\"");
+        } else {
+            System.out.println("catName is not equal to \"Shane\"");
+        }
+
+        if (catName == catName2) {
+            System.out.println("catName is equal to catName2");
+        } else {
+            System.out.println("catName is not equal to catName2");
+        }
+
+        if (catName == catName3) {
+            System.out.println("catName is equal to catName3");
+        } else {
+            System.out.println("catName is not equal to catName3");
+        }
+
+        if (catName.equals(catName3)) {
+            System.out.println("catName is equal to catName3");
+        } else {
+            System.out.println("catName is not equal to catName3");
+        }
     }
 }
