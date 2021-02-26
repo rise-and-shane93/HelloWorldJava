7db05316c45167df61bafb81cadf4f14b8f71022
Shane Harper <cdtharper@gmail.com>
2021-02-26 18:41:39 -0500

    trying string methods

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,18 +1,15 @@
 public class HelloWorld {
     public static void main(String[] args) {
         
-        // int[] intArray = {1,2,3,4,5};
-        // int intSum = 0;
+        String street = "316 Chumleigh Rd.";
+        String city = "Baltimore";
+        String state = "MD";
+        String zip5 = "21212";
+        String mailingAddr = "100 Burke Ave.;Towson;MD;21204";
 
-        // for (int tempInt = 0; tempInt < intArray.length; tempInt++) {
-        //     intSum += intArray[tempInt];
-        // }
-
-        // for (int tempInt : intArray) {
-        // Process all elements of tempArray
-        //     intSum += tempInt;
-        // }
-
-        // System.out.println(intSum);
+        mailingAddr = String.join(",",street,city,state,zip5);
+        mailingAddr = mailingAddr.replace(",","/");
+        System.out.println("Mailing Addres: " + mailingAddr);
+        System.out.println("City/State/Zip: " + mailingAddr.substring(mailingAddr.indexOf("/") + 1).replace("/", ""));
     }
 }
